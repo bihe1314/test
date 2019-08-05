3e8e24cfee3642d1f055ee8a6b57c444daeb093c
bihe <bihe@bihe-PC>
2019-08-05 10:02:11 +0800

    gh

--- src/aaa/aa.java
+++ src/aaa/aa.java
@@ -4,7 +4,7 @@ public class aa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println();
+System.out.println(33);
 System.out.println(111111111);
 	}
 
